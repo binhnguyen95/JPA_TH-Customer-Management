2708ad82ecceececc9843ebec6326173260f55c4
binhnguyen95 <invalidmyarse@gmail.com>
2022-09-15 09:36:50 +0700

    done

---
+++ src/main/java/com/example/jpa_thcustomermanagement/repository/ICustomerRepository.java
@@ -0,0 +1,6 @@
+package com.example.jpa_thcustomermanagement.repository;
+
+import com.example.jpa_thcustomermanagement.model.Customer;
+
+public interface ICustomerRepository extends IGeneralRepository<Customer> {
+}
---
+++ src/main/java/com/example/jpa_thcustomermanagement/service/ICustomerService.java
@@ -0,0 +1,6 @@
+package com.example.jpa_thcustomermanagement.service;
+
+import com.example.jpa_thcustomermanagement.model.Customer;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
